3a55792bd8cf90508039a6e9f9fb293c0f22b713
maorRoz <andorys@post.bgu.ac.il>
2018-11-05 09:58:31 +0200

    removed unnecessary $()

--- src/js/view/elementRowUI.js
+++ src/js/view/elementRowUI.js
@@ -14,7 +14,6 @@ const rowToHtml = (row) => {
 };
 
 export const createHtmlElementRow = (row) => {
-    $()
     const htmlRow = document.createElement('tr');
     const { htmlLineNum,htmlLineType,htmlLineName, htmlLineValue } = rowToHtml(row);
     htmlRow.append(htmlLineNum,htmlLineType,htmlLineName, htmlLineValue);
